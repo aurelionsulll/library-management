52c37bb562cbcd45f9686e5de175f77496b35fc8
aurelionsulll <mennanamehdi@gmail.com>
2021-04-21 16:54:25 +0000

    gitchange

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Git is working
+
                 </div>
 
                 <div class="links">
